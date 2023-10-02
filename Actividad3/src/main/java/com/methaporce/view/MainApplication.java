50380e2670dc27fc2028e3d792e902ab083cef1d
Narr the Reg <juangerman-13@hotmail.com>
2023-10-02 13:47:40 -0600

    start

---
+++ Actividad3/src/main/java/com/methaporce/view/MainApplication.java
@@ -0,0 +1,9 @@
+package com.methaporce.view;
+
+public class MainApplication {
+
+	public static void main(String[] args) {
+        System.out.println("Hello old World!"); // Display the string.
+	}
+
+}
--- Actividad3/src/main/java/com/methaporce/view/MainMovieView.java
+++
@@ -1,7 +0,0 @@
-package com.methaporce.view;
-
-public class MainMovieView {
-    public static void main(String args[]) {  
-        System.out.println("Static method");  
-    }  
-}
