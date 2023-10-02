9bf66455e7c9b0a68635da877294b11592cca83d
Narr the Reg <juangerman-13@hotmail.com>
2023-10-02 15:26:22 -0600

    Implement movie and movie controller

--- Actividad3/src/main/java/com/methaporce/model/Movie.java
+++ Actividad3/src/main/java/com/methaporce/model/Movie.java
@@ -1,5 +1,39 @@
 package com.methaporce.model;
 
 public class Movie {
+    private final int id;
+    private String name;
+    private boolean available;
 
+    Movie(int id, String name) {
+        this.id = id;
+        this.name = name;
+        this.available = false;
+    }
+
+    Movie(int id, String name, boolean available) {
+        this.id = id;
+        this.name = name;
+        this.available = available;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setName(String new_name) {
+        this.name = new_name;
+    }
+
+    public void setAvailability(boolean is_available) {
+        this.available = is_available;
+    }
 }
--- Actividad3/src/main/java/com/methaporce/model/MovieController.java
+++ Actividad3/src/main/java/com/methaporce/model/MovieController.java
@@ -1,5 +1,80 @@
 package com.methaporce.model;
 
+import java.util.ArrayList;
+
 public class MovieController {
+    private ArrayList<Movie> movies = new ArrayList<Movie>();
+
+    public ArrayList<Movie> GetMovies() {
+        return movies;
+    }
+
+    public ArrayList<Movie> GetAvailableMovies() {
+        ArrayList<Movie> available_movies = new ArrayList<Movie>();
+
+        for (Movie movie : movies) {
+            if (movie.isAvailable()) {
+                available_movies.add(movie);
+            }
+        }
+
+        return available_movies;
+    }
+
+    public ArrayList<Movie> GetUnavailableMovies() {
+        ArrayList<Movie> non_available_movies = new ArrayList<Movie>();
+
+        for (Movie movie : movies) {
+            if (!movie.isAvailable()) {
+                non_available_movies.add(movie);
+            }
+        }
+
+        return non_available_movies;
+    }
+
+    public Movie GetMovieById(int id) {
+        for (Movie movie : movies) {
+            if (id != movie.getId()) {
+                continue;
+            }
+            return movie;
+        }
+
+        // Movie not found
+        return null;
+    }
+
+    public boolean MarkMovieAsAvailable(int id) {
+        Movie movie = GetMovieById(id);
+
+        if (movie == null) {
+            // Id doesn't exist
+            return false;
+        }
+
+        movie.setAvailability(true);
+        return true;
+    }
+
+    public boolean AddMovie(Movie new_movie) {
+        if (GetMovieById(new_movie.getId()) != null) {
+            // Movie already exist
+            return false;
+        }
+
+        return movies.add(new_movie);
+    }
+
+    public boolean deleteMovie(int id) {
+        Movie movie = GetMovieById(id);
+
+        if (movie == null) {
+            // Id doesn't exist
+            return false;
+        }
+
+        return movies.remove(movie);
+    }
 
 }
